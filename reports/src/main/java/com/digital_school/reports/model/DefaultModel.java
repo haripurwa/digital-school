cdef90d435899086014b69d576486bfcc8b3a142
Hari Purwanto <haripurwanto446@gmail.com>
2024-07-15 02:32:52 +0700

    fix: base model

---
+++ reports/src/main/java/com/digital_school/reports/model/DefaultModel.java
@@ -0,0 +1,38 @@
+package com.digital_school.reports.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class DefaultModel implements Serializable {
+
+    @Id
+    @Column(columnDefinition = "varchar(36)")
+    private String id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        id= String.valueOf(UUID.randomUUID());
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
